dfc96b10e5559abbb3398b203bc8f7b752b5bdb9
panda <91168831+pandacoder123@users.noreply.github.com>
2023-04-06 16:22:16 -0700

    Update sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,9 +1,11 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
+      '/pwa-examples/a2hs/',
       '/pwa-examples/a2hs/index.html',
       '/pwa-examples/a2hs/index.js',
       '/pwa-examples/a2hs/style.css',
+      '/pwa-examples/a2hs/images/fox1.jpg',
     ])),
   );
 });
