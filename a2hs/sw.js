c387d57c42ee037649e502e6a1bb8f5ccd961202
panda <91168831+pandacoder123@users.noreply.github.com>
2023-04-06 17:43:44 -0700

    Update sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,18 +1,18 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      '/calculator/a2hs/',
-      '/calculator/a2hs/index.html',
-      '/calculator/a2hs/index.js',
-      '/calculator/a2hs/style.css',
-      '/calculator/a2hs/icon/fox.jpg',
-    ])),
-  );
-});
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('fox-store').then((cache) => cache.addAll([
+//       '/calculator/a2hs/',
+//       '/calculator/a2hs/index.html',
+//       '/calculator/a2hs/index.js',
+//       '/calculator/a2hs/style.css',
+//       '/calculator/a2hs/icon/fox.jpg',
+//     ])),
+//   );
+// });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+// self.addEventListener('fetch', (e) => {
+//   console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request)),
+//   );
+// });
